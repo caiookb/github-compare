2f2018f9b7b9fc23dc3fd565f85800cc16bd04e7
Caio <caiookb@gmail.com>
2020-12-06 19:05:00 -0300

    feature(Button): Added type to receive on props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,11 +2,12 @@ import React from "react";
 import Icon from "../Icon/Icon";
 import styles from "./Button.module.css";
 
-const Button = ({ title, icon, btnClass, onClick }) => {
+const Button = ({ title, icon, btnClass, onClick, type }) => {
   return (
     <button
       className={`btn btn-${btnClass} ${styles.button}`}
       onClick={onClick}
+      type={type}
     >
       {title}
       {icon ? <Icon icon={icon} /> : null}
