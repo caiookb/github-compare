90b518835af4c5d24a27107a54df9b5e8af00862
Caio <caiookb@gmail.com>
2020-12-06 19:02:34 -0300

    feature/refactor(Main): Added ExpandedCard to render and setting up userOptions on first render

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,35 +1,44 @@
 import React, { useEffect } from "react";
-import { Card } from "../../components";
+import { Card, ExpandedCard } from "../../components";
 import Nav from "../Nav/Nav";
-import moment from "moment";
 import EmptyRepositories from "../EmptyRepositories/EmptyRepositories";
 import { connect } from "react-redux";
 import * as RepositoryController from "../../controllers/RepositoriesController";
+import styles from "./Main.module.css";
+import { OptionsController } from "../../controllers";
 
 const Main = (props) => {
-  const { repositoriesList, saveLocalRepoOnRedux } = props;
+  const {
+    repositoriesList,
+    saveLocalRepoOnRedux,
+    cardView,
+    setUpOptions,
+  } = props;
 
   useEffect(() => {
     saveLocalRepoOnRedux();
+    setUpOptions();
   }, []);
 
   const empty = repositoriesList?.length === 0;
-
-  console.log("emp", empty);
 
   return (
     <div className={"container-fluid"}>
       <Nav />
 
       <div className="container">
-        <div className="row">
+        <div className={`row  ${styles.view}`}>
           {empty ? (
             <EmptyRepositories />
           ) : (
             <>
-              {repositoriesList?.map((repository) => (
-                <Card repository={repository} />
-              ))}
+              {repositoriesList?.map((repository) =>
+                cardView === "cards" ? (
+                  <Card repository={repository} />
+                ) : (
+                  <ExpandedCard repository={repository} />
+                )
+              )}
             </>
           )}
         </div>
@@ -41,14 +50,16 @@ const Main = (props) => {
 const mapStateToProps = (state) => {
   const {
     repositories: { repositoriesList },
+    userOptions: { cardView },
   } = state;
-  return { repositoriesList };
+  return { repositoriesList, cardView };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     saveLocalRepoOnRedux: () =>
       dispatch(RepositoryController.saveLocalRepoOnRedux()),
+    setUpOptions: () => dispatch(OptionsController.setupUserOptions()),
   };
 };
 
