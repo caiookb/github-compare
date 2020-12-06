d30310a3538b5b1c648d462ea0a2410d869e513b
Caio <caiookb@gmail.com>
2020-12-06 04:51:30 -0300

    feature(Components): Created Icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { icons } from "../../assets/icons";
+
+const Icon = ({
+  icon,
+  padding,
+  backgroundColor,
+  color,
+  radius,
+  onClick,
+  ref,
+}) => {
+  return (
+    <i
+      style={{
+        color: color,
+        backgroundColor: backgroundColor,
+        padding: `${padding - 2}px ${padding}px`,
+        borderRadius: radius,
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+    >
+      <svg class="lexicon-icon" focusable="false" role="presentation">
+        <use xlinkHref={`${icons}#${icon}`} />
+      </svg>
+    </i>
+  );
+};
+
+export default Icon;
+
+Icon.defaultProps = {
+  color: "#6B6C7E",
+  backgroundColor: "unset",
+  padding: 5,
+  radius: 0,
+};
