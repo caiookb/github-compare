aee0be7d7c971619eb5622bea7ef5f508fb1d357
Caio <caiookb@gmail.com>
2020-12-06 19:04:06 -0300

    refactor(TextInput): Added error return to input

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "../Icon/Icon";
 import styles from "./TextInput.module.css";
 
-const TextInputIcon = ({ icon }) => {
+const TextInputIcon = ({ icon, onClick }) => {
   return (
     <div class="input-group-inset-item input-group-inset-item-after">
       <button class="btn btn-unstyled d-none d-md-inline-block" type="button">
-        <Icon icon={icon} />
-      </button>
+        <Icon icon={icon} onClick={onClick} />
+      </button>{" "}
     </div>
   );
 };
 
 const TextInput = (props) => {
-  const { placeholder, icon, value, onChange, width, label } = props;
+  const [value, setValue] = useState("");
+  const { placeholder, icon, onChange, label, width, required, error } = props;
+
   return (
     <div class="input-group">
       <div class="input-group-item">
-        <label>{label}</label>
+        <label>
+          {label}{" "}
+          {required ? <span style={{ color: "#B95000" }}>*</span> : null}
+        </label>
+
         <input
           placeholder={placeholder}
           value={value}
-          onChange={(e) => onChange(e.target.value)}
-          className={`form-control input-group-inset ${
-            icon ? " input-group-inset-after" : ""
-          } ${styles.input}`}
-          style={{ width: width }}
+          onChange={(e) => {
+            setValue(e.target.value);
+            onChange(e.target.value);
+          }}
+          style={{ width }}
+          className={`form-control input-group-inset  ${
+            icon ? "input-group-inset-after" : null
+          } ${error ? styles.error : null} ${styles.input}`}
         />
-        {icon ? <TextInputIcon icon={icon} /> : null}
+        {icon ? (
+          <TextInputIcon onClick={() => onChange(value)} icon={icon} />
+        ) : null}
       </div>
     </div>
   );
