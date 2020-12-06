ee03c8eb16bbfdd27983a2c3412a23c00292c3fb
Caio <caiookb@gmail.com>
2020-12-06 04:54:41 -0300

    feature(Components): Updating componetns import file with new ones

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Card from "./Card/Card";
+import Button from "./Button/Button";
+import Dropdown from "./Dropdown/Dropdown";
+import Icon from "./Icon/Icon";
+import InputDropdown from "./InputDropdown/InputDropdown";
 import Tag from "./Tag/Tag";
+import TextInput from "./TextInput/TextInput";
 
-export { Card, Tag };
+export { Button, Card, Dropdown, Icon, InputDropdown, Tag, TextInput };
