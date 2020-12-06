4d96e4afc4e6c755bed022fdbfa12fa62cd41c92
Caio <caiookb@gmail.com>
2020-12-06 19:10:32 -0300

    feature(Components): Added ExpandedCard and Modal to import file

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,21 @@
 import Card from "./Card/Card";
 import Button from "./Button/Button";
 import Dropdown from "./Dropdown/Dropdown";
+import ExpandedCard from "./ExpandedCard/ExpandedCard";
 import Icon from "./Icon/Icon";
 import InputDropdown from "./InputDropdown/InputDropdown";
+import Modal from "./Modal/Modal";
 import Tag from "./Tag/Tag";
 import TextInput from "./TextInput/TextInput";
 
-export { Button, Card, Dropdown, Icon, InputDropdown, Tag, TextInput };
+export {
+  Button,
+  Card,
+  Dropdown,
+  ExpandedCard,
+  Icon,
+  InputDropdown,
+  Modal,
+  Tag,
+  TextInput,
+};
